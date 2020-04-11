468017a6a1c61eb9a2f8a1be95d398cac353328b
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-04-11 00:45:08 -0600

    added questions/answers

---
+++ script.js
@@ -0,0 +1,49 @@
+var myQuestions = [{
+    q: "Commonly used data types do not include?",
+    a: {
+        1: "strings",
+        2: "booleans",
+        3: "alerts",
+        4: "numbers",
+    },
+    correctAnswer: "2"
+
+    q: "Arrays in javascript can be used to store?",
+    a: {
+        1: "numbers and strings",
+        2: "other arrays",
+        3: "booleans",
+        4: "all of the above",
+    },
+    correctAnswer: "4"
+
+    q: "The condition in an if/else statement is enclosed with?",
+    a: {
+        1: "quotes",
+        2: "curly brackets",
+        3: "parenthesis",
+        4: "square brackets",
+    },
+    correctAnswer: "3"
+
+    q: "A very useful tool used during development and debugging for printing content to the debugger is?",
+    a: {
+        1: "javascript",
+        2: "terminal/bash",
+        3: "for loops",
+        4: "console.log",
+    },
+    correctAnswer: "3"
+
+    q: "What must string values be enclosed in when being assigned to variables?",
+    a: {
+        1: "commas",
+        2: "curly brackets",
+        3: "quotes",
+        4: "parenthesis",
+    },
+    correctAnswer: "2"
+}
+
+object.onclick = function () {btnPush};
+object.addEventListener("click", btnPush);
